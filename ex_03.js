f047eba634ce7966db64cb915e0423fa42680691
Fatima-web06 <fatima.fall@epitech.digital>
2024-10-07 13:18:57 +0200

    Create ex_03.js

---
+++ ex_03.js
@@ -0,0 +1,23 @@
+function addCarToAgent(agent, brand, model, color, power) {
+    if (typeof agent !== 'object' || typeof brand !== 'string' || typeof model !== 'string' || typeof color !== 'string' || typeof power !== 'string') { 
+        return null; 
+    }
+
+    agent.car = { brand, model, color, power };
+    return agent; 
+}
+
+let agent = { 
+    firstName: 'James', 
+    lastName: 'Bond'    
+};
+
+agent = addCarToAgent(agent, 'Aston Martin', 'DB5', 'Grey', '285bhp');
+
+
+if (agent) { 
+    shareThatBeauty(agent); 
+}
+
+
+
